723bc56f8af6fbd06033a0dcea55db7af4172566
daskza19 <josep.sanchez.arbona@gmail.com>
2021-05-04 13:24:12 +0200

    Cases amb interior i prefab de la taula

---
+++ AudioUnityPractice/Assets/Scripts/ShowDialoguePanel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro.EditorUtilities;
+
+public class ShowDialoguePanel : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Adeu Dialeg");
+            GetComponent<Animator>().SetTrigger("BocadilloIn");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Adeu Dialeg");
+            GetComponent<Animator>().SetTrigger("BocadilloOut");
+        }
+    }
+}
