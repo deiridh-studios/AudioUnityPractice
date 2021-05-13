02fdac99c3e8f35fdd016a68fd335743bc1b01da
Yuye <47743107+Rugiacreed@users.noreply.github.com>
2021-05-13 14:45:19 +0200

    Added some cute things skull and others

---
+++ AudioUnityPractice/Assets/Scripts/AudioTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTrigger : MonoBehaviour
+{
+    public AudioSource playSound;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        playSound.Play();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        playSound.Stop();
+    }
+}
---
+++ AudioUnityPractice/Assets/Scripts/SkullPickUp.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkullPickUp : MonoBehaviour
+{
+    public Rigidbody rb;
+    public CapsuleCollider cc;
+    public Transform player, skullcontainer;
+    public AudioSource skullaudio;
+    public AudioSource evilinside;
+
+    public float pickuprange;
+
+    public bool carrying;
+
+    private void Update()
+    {
+        Vector3 distanceToPlayer = player.position - transform.position;
+        if (!carrying && distanceToPlayer.magnitude <= pickuprange && Input.GetKeyDown(KeyCode.E)) PickUp();
+    }
+    private void PickUp()
+    {
+        carrying = true;
+
+        transform.SetParent(skullcontainer);
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.Euler(Vector3.zero);
+        transform.localScale = new Vector3(35, 35, 35);
+
+
+        skullaudio.Stop();
+        evilinside.Play();
+
+    }
+}
